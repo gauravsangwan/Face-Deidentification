e1a2a0473843b05216b2bf2dce04645a57924755
Gaurav Sangwan <sangwan.2@iitj.ac.in>
2022-05-13 05:10:05 +0530

    Configs

--- Models/Encoders/id_encoder.py
+++ Models/Encoders/id_encoder.py
@@ -1,6 +1,16 @@
 import torch
 import torch.nn
+from facenet_pytorch import MTCNN, InceptionResnetV1
+from torchvision import transforms
+
 IMAGE_SIZE = 220
+mtcnn = MTCNN(
+    image_size = IMAGE_SIZE, margin = 0 ,
+    thresholds = [0.6, 0.7, 0.7], factor = 0.709, post_process = True,
+    device = 
+)
+
+
 
 class Id_encoder(nn.Module):
     def __init__(self):
