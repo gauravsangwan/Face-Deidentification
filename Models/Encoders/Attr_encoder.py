6daf7e1d23566d0e773ff709190bba7d66f6d589
Gaurav Sangwan <sangwan.2@iitj.ac.in>
2022-05-13 04:55:14 +0530

    attr encoder

---
+++ Models/Encoders/Attr_encoder.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn
+import torch.utils.data 
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+
+class Encoder_attributes(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+class Identity(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        return x
