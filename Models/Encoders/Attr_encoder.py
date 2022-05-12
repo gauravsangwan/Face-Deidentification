37dd8cd6d45095299f492ba7f9bf1fab9021043a
Gaurav Sangwan <sangwan.2@iitj.ac.in>
2022-05-13 05:00:23 +0530

    attr_encoder complete

--- Models/Encoders/Attr_encoder.py
+++ Models/Encoders/Attr_encoder.py
@@ -5,15 +5,24 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 
 class Encoder_attributes(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self,pretrained = False):
+        # pass
+        super(Encoder_attributes,self).__init__()
+        self.model = torch.hub.load('pytorch/vision:v0.6.0', 'inception_v3', pretrained=pretrained, aux_logits=False, init_weights=False)
+        self.model.fc = Identity()
+        self.meta = { 'mean': [0.485, 0.456, 0.406],
+                      'std': [0.229, 0.224, 0.225],
+                      'input_size': [299, 299,3]
+                    #   'input_size': [3,299, 299]
+                      }
 
     def forward(self, x):
-        pass
+        return self.model(x)
 
 class Identity(nn.Module):
     def __init__(self):
-        pass
+        # pass
+        super(Identity,self).__init__()
 
     def forward(self, x):
         return x
