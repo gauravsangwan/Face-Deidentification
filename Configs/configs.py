48dfb37b79143aae7f3a31b7384f140ac4d0f175
Gaurav Sangwan <sangwan.2@iitj.ac.in>
2022-05-13 05:10:39 +0530

    configs

---
+++ Configs/configs.py
@@ -0,0 +1,9 @@
+import torch
+
+run_in_colab = False
+run_in_notebook = False
+run_in_slurm = True
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+step = 0
+BASE_PATH = ''
+# print(device)
