0791ee482639abeba2c147df74ddf9d6bb23577a
Gaurav Sangwan <sangwan.2@iitj.ac.in>
2022-05-13 06:36:13 +0530

    inception.py completed

--- Models/Encoders/inception.py
+++ Models/Encoders/inception.py
@@ -6,7 +6,20 @@ import torchvision.transforms as transforms
 
 class Inception(nn.Module):
     def __init__(self):
-        pass
+        # pass
+        super(Inception,self).__init__()
+        full_inception = torch.hub.load('pytorch/vision:v0.6.0', 'inception_v3', pretrained=True, aux_logits=False, init_weights=False)
+        removed = list(full_inception.children())[:-1]
+        self.model = nn.Sequential(*removed)
+        # self.meta = { 'mean': [0.485, 0.456, 0.406],
+        #               'std': [0.229, 0.224, 0.225],
+        #               'input_size': [299, 299,3]
+        #             #   'input_size': [3,299, 299]
+        #               }
+        self.preprocess = transforms.Compose([transforms.Resize(299),
+                                                transforms.CenterCrop(299)])
 
     def forward(self, data):
-        pass
+        # pass
+        resized_data = self.preprocess(data)
+        return self.model(resized_data * 255)
