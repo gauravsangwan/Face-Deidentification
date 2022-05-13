fadc4a6d3130de54c07aaeb226cf7be4cdd4d0c9
Gaurav Sangwan <sangwan.2@iitj.ac.in>
2022-05-13 09:11:25 +0530

    landmark enc done

--- Models/Encoders/Landmark_encoder/Landmark_encoder.py
+++ Models/Encoders/Landmark_encoder/Landmark_encoder.py
@@ -2,11 +2,28 @@ from torch
 from .mobile_facenet import MobileFaceNet
 from torchvision import transforms
 
+PATH = 'Weights/mobilefacenet.pth.tar'
+
 class Encoder(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self,model_dir = PATH):
+        # pass
+        super(Encoder, self).__init__()
+        self.model = MobileFaceNet([112,112],136)
+        checkpoint = torch.load(model_dir)
+        self.model.load_state_dict(checkpoint['state_dict'])
+        self.model = self.model.eval()
+        self.resize = transforms.Resize(112)
+
+    def preprocess(self,imgs):
+        return self.resize(imgs)
 
     def forward(self):
-        pass
+        # pass
+        resized_image = self.preprocess(imgs)
+        outputs, _ = self.model(resized_image)
 
-    
+        batch_size = resized_image.shape[0]
+        landmarks = torch.reshape(outputs*112,(batch_size,68,2))
+
+        return outputs*112, landmarks[:,17:,:]
+
