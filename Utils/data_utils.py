f1e0f5f5736d119c508955050dfe5980a4ad848e
Gaurav Sangwan <sangwan.2@iitj.ac.in>
2022-05-13 12:09:17 +0530

    utils

---
+++ Utils/data_utils.py
@@ -0,0 +1,11 @@
+from tkinter import Image
+import matplotlib.pyplot as plt
+import torch
+import os
+import numpy as np
+from torch.utils.data import Dataset
+for PIL import Image
+from torchvision import transforms
+from Configs import configs
+
+to_tensor_transform = transforms.ToTensor()
