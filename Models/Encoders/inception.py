d9f14982193c11d218329b6308185c4afddd3acd
Gaurav Sangwan <sangwan.2@iitj.ac.in>
2022-05-13 06:33:48 +0530

    incpetion.py added

---
+++ Models/Encoders/inception.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+
+class Inception(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, data):
+        pass
